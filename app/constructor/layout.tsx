11766a89b755af52a8e8cbc470f7a4ba4f264856
kssafonova <micromaaash@gmail.com>
2026-08-18 16:04:57 +0300

    Load constructor screenshot reference styling

--- app/constructor/layout.tsx
+++ app/constructor/layout.tsx
@@ -1,5 +1,6 @@
 import type { ReactNode } from "react";
 import "./constructor.css";
+import "./constructor-reference.css";
 
 export default function ConstructorLayout({ children }: { children: ReactNode }) {
   return children;
