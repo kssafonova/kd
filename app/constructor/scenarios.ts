1a6dbf7a64749edd694747529fd172c696630d40
kssafonova <micromaaash@gmail.com>
2026-08-18 05:58:21 +0300

    Switch constructor to final 11 scenarios

--- app/constructor/scenarios.ts
+++ app/constructor/scenarios.ts
@@ -1,9 +1,15 @@
 export const CONSTRUCTOR_SCENARIO_IDS = [
-  "red-thread-tea",
-  "quiet-obereg",
-  "sky-celebration",
-  "green-salon",
-  "blue-hour-bedroom",
+  "winter-garden-breakfast",
+  "earth-breath",
+  "northern-dreams-bedroom",
+  "fairy-tea",
+  "botanical-garden",
+  "retro-cabinet",
+  "blue-velvet-night",
+  "cloud-tenderness",
+  "mokoshi-morning",
+  "kokoshnik-gift",
+  "forest-fairytale-obereg",
 ] as const;
 
 export type ConstructorScenarioId = (typeof CONSTRUCTOR_SCENARIO_IDS)[number];
