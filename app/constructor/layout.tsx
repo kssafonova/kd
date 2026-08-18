0fb5ff2f33a7f459cec9b541fe1acdfc970c086a
kssafonova <micromaaash@gmail.com>
2026-08-18 16:08:57 +0300

    Rollback constructor screenshot reference styling

--- app/constructor/layout.tsx
+++ app/constructor/layout.tsx
@@ -1,6 +1,5 @@
 import type { ReactNode } from "react";
 import "./constructor.css";
-import "./constructor-reference.css";
 
 export default function ConstructorLayout({ children }: { children: ReactNode }) {
   return children;
