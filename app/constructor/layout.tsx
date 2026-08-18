67bd61ffb39ad98e0136edaace1167f3aabf58a6
kssafonova <micromaaash@gmail.com>
2026-08-18 06:57:24 +0300

    Remove premium system import from constructor

--- app/constructor/layout.tsx
+++ app/constructor/layout.tsx
@@ -1,6 +1,5 @@
 import type { ReactNode } from "react";
 import "./constructor.css";
-import "../premium-system.css";
 
 export default function ConstructorLayout({ children }: { children: ReactNode }) {
   return children;
