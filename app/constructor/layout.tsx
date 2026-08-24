dfafaf507fec811c0dad53a0a7e708dc51e53edb
kssafonova <micromaaash@gmail.com>
2026-08-24 23:54:13 +0300

    Load ready solution builder styles

--- app/constructor/layout.tsx
+++ app/constructor/layout.tsx
@@ -2,6 +2,7 @@ import type { ReactNode } from "react";
 import "./constructor.css";
 import "./simple-flow-v22.css";
 import "./table-solutions-v24.css";
+import "./table-solution-builder-v25.css";
 
 export default function ConstructorLayout({ children }: { children: ReactNode }) {
   return children;
