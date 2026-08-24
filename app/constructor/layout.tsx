02f8a529ae8b95e59946f1588d665be22e04798a
kssafonova <micromaaash@gmail.com>
2026-08-24 22:44:03 +0300

    Load simplified ready solutions styles

--- app/constructor/layout.tsx
+++ app/constructor/layout.tsx
@@ -1,5 +1,6 @@
 import type { ReactNode } from "react";
 import "./constructor.css";
+import "./simple-flow-v22.css";
 
 export default function ConstructorLayout({ children }: { children: ReactNode }) {
   return children;
