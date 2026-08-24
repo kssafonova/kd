c3ed663fe8d4dd46d12595326b0c6342289e0faf
kssafonova <micromaaash@gmail.com>
2026-08-24 23:56:07 +0300

    Load ready solution finder styles

--- app/constructor/layout.tsx
+++ app/constructor/layout.tsx
@@ -3,6 +3,7 @@ import "./constructor.css";
 import "./simple-flow-v22.css";
 import "./table-solutions-v24.css";
 import "./table-solution-builder-v25.css";
+import "./table-solutions-finder-v26.css";
 
 export default function ConstructorLayout({ children }: { children: ReactNode }) {
   return children;
