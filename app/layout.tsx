0be2bc67db1b8930a5a878ffd16a58ca3a8b7925
kssafonova <micromaaash@gmail.com>
2026-08-24 19:10:53 +0300

    Load approved homepage header reference last

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,6 +28,7 @@ import "./home-ux-v11.css";
 import "./home-ux-v11-mobile.css";
 import "./home-video-v11.css";
 import "./gift-wrap-flow.css";
+import "./home-header-reference-v12.css";
 import { ProductCardGalleryEnhancer } from "./product-card-gallery";
 
 export const metadata: Metadata = {
