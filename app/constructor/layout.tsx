3078f475094119a2e176fb3c0c77928965cd8f06
kssafonova <micromaaash@gmail.com>
2026-08-24 23:22:45 +0300

    Load table ready solutions V24 styles

--- app/constructor/layout.tsx
+++ app/constructor/layout.tsx
@@ -1,6 +1,7 @@
 import type { ReactNode } from "react";
 import "./constructor.css";
 import "./simple-flow-v22.css";
+import "./table-solutions-v24.css";
 
 export default function ConstructorLayout({ children }: { children: ReactNode }) {
   return children;
