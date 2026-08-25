e7d6f12f91d96d993b95274123e4506ceeccdb40
kssafonova <micromaaash@gmail.com>
2026-08-25 14:09:17 +0300

    Import Ready Solutions V63 styles

--- app/ready-solutions/layout.tsx
+++ app/ready-solutions/layout.tsx
@@ -3,6 +3,7 @@ import "./ready-solutions-v57.css";
 import "./ready-solutions-v60.css";
 import "./ready-solutions-v61.css";
 import "./ready-solutions-v62.css";
+import "./ready-solutions-v63.css";
 
 export default function ReadySolutionsLayout({ children }: { children: React.ReactNode }) {
   return children;
