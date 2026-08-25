4b9cf4f1242624ccb4b0e8c702751b66fa449d8d
kssafonova <micromaaash@gmail.com>
2026-08-25 19:38:22 +0300

    Activate V71 ready solution wizard

--- app/ready-solutions/ready-solutions-client.tsx
+++ app/ready-solutions/ready-solutions-client.tsx
@@ -1,1 +1,2 @@
-export { ReadySolutionsLanding, ReadySolutionWizard } from "./ready-solutions-v57-client";
+export { ReadySolutionsLanding } from "./ready-solutions-v57-client";
+export { ReadySolutionWizard } from "./ready-solutions-v71-client";
