b2966e1f26b989bd66e28ebdb8c1c13e38393289
kssafonova <micromaaash@gmail.com>
2026-08-25 15:29:09 +0300

    Import ready solutions V66 editorial styles

--- app/ready-solutions/layout.tsx
+++ app/ready-solutions/layout.tsx
@@ -5,6 +5,7 @@ import "./ready-solutions-v61.css";
 import "./ready-solutions-v62.css";
 import "./ready-solutions-v63.css";
 import "./ready-solutions-v64.css";
+import "./ready-solutions-zara-kultura-v66.css";
 
 export default function ReadySolutionsLayout({ children }: { children: React.ReactNode }) {
   return children;
