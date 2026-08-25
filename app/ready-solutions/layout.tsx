ec77e97bc03b8e86258a9c89f39238687503bb39
kssafonova <micromaaash@gmail.com>
2026-08-25 18:27:58 +0300

    Import ready solutions V68 styles

--- app/ready-solutions/layout.tsx
+++ app/ready-solutions/layout.tsx
@@ -6,6 +6,7 @@ import "./ready-solutions-v62.css";
 import "./ready-solutions-v63.css";
 import "./ready-solutions-v64.css";
 import "./ready-solutions-zara-kultura-v66.css";
+import "./ready-solutions-v68.css";
 
 export default function ReadySolutionsLayout({ children }: { children: React.ReactNode }) {
   return children;
