50d8ca50d2042ec129822bc3d7721a1ea8c60798
kssafonova <micromaaash@gmail.com>
2026-08-25 10:50:29 +0300

    Load unified collection story styles V52

--- app/layout.tsx
+++ app/layout.tsx
@@ -46,6 +46,7 @@ import "./collections-catalog-parity-v34.css";
 import "./collections-editorial-v48.css";
 import "./collections-landing-v49.css";
 import "./collections-zara-kultura-v50.css";
+import "./unified-stories-v52.css";
 import "./commerce-zara-kultura-v41.css";
 import "./commerce-hypotheses-v42.css";
 import "./commerce-clarity-v43.css";
