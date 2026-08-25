6de4bacdf2a80b1f56262e678754ca0bcf5bd2a9
kssafonova <micromaaash@gmail.com>
2026-08-25 18:08:47 +0300

    Import cart checkout V69 styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -49,6 +49,7 @@ import "./collections-v65.css";
 import "./collections-zara-kultura-v66.css";
 import "./mobile-cart-checkout-v67.css";
 import "./one-screen-checkout-v68.css";
+import "./cart-checkout-mockup-v69.css";
 import { ProductCardGalleryEnhancer } from "./product-card-gallery";
 import { CollectionPurchaseEnhancer } from "./collection-purchase-enhancer";
 import { ProfileAddressBookEnhancer } from "./profile-address-book";
