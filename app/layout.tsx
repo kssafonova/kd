397697a10d43c2d2043d45099346ed963cf192aa
kssafonova <micromaaash@gmail.com>
2026-08-25 04:13:44 +0300

    Load hypothesis-driven commerce V42 styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,6 +44,7 @@ import "./collections-zara-kultura-v31.css";
 import "./collections-zara-kultura-v32.css";
 import "./collections-catalog-parity-v34.css";
 import "./commerce-zara-kultura-v41.css";
+import "./commerce-hypotheses-v42.css";
 import { ProductCardGalleryEnhancer } from "./product-card-gallery";
 import { CollectionPurchaseEnhancer } from "./collection-purchase-enhancer";
 import { ProfileAddressBookEnhancer } from "./profile-address-book";
