75e8fa967123eeeba22836c19264d5f61ac6b478
kssafonova <micromaaash@gmail.com>
2026-08-25 10:53:30 +0300

    Clean constructor layout and load V52 styles

--- app/constructor/layout.tsx
+++ app/constructor/layout.tsx
@@ -7,12 +7,7 @@ import "./table-solutions-finder-v26.css";
 import "./table-solution-type-constructor-v27.css";
 import "./table-solution-multiselect-v28.css";
 import "./table-solution-kultura-editorial-v29.css";
-import "./ready-solutions-landing-v30.css";
-import "./ready-solutions-site-v33.css";
-import "./ready-solutions-zara-kultura-v38.css";
-import "./ready-solutions-hero-v39.css";
-import "./ready-solutions-constructor-v46.css";
-import "./ready-solutions-v47.css";
+import "./ready-solutions-v52.css";
 
 export default function ConstructorLayout({ children }: { children: ReactNode }) {
   return children;
