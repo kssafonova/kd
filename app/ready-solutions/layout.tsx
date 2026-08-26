cf5609be0c9d7c7d16c534ae9890629503abd104
kssafonova <micromaaash@gmail.com>
2026-08-26 12:32:35 +0300

    Load V80 ready solutions refinements

--- app/ready-solutions/layout.tsx
+++ app/ready-solutions/layout.tsx
@@ -12,6 +12,7 @@ import "./ready-solutions-v70.css";
 import "./ready-solutions-v71.css";
 import "./ready-solutions-v78-responsive.css";
 import "./ready-solutions-v79-adaptive.css";
+import "./ready-solutions-v80.css";
 
 export default function ReadySolutionsLayout({ children }: { children: React.ReactNode }) {
   return children;
