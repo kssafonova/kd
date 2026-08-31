24e800cb644a6684d92e2a9b6ae99aa91876e8bc
kssafonova <micromaaash@gmail.com>
2026-08-31 04:39:16 +0300

    Keep home components aligned with Kultura UI system

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import "./menu-zara-premium.css";
 import "./home-standalone.css";
+import "./kultura-design-system-v160.css";
 import HomeStandalone from "./home-standalone";
 
 export default function HomePage(){
